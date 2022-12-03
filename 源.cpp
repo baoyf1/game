c6e46d3866294932589d5074396913a70a474379
ku <113022697+baoyf1@users.noreply.github.com>
2022-12-03 17:30:19 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,1 +1,22 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int num = 10;
+int main()
+{
+	int num = 10;
+	printf("加入比特学习\n");
+	printf("敲代码");
+	int line = 0;
+	while (line < 20000)
+	{
+
+		printf("line:%d\n", line);
+		line++;
+	}
+	if (line == 20000)
+	{
+		printf("好offer");
+	}
+
+	return 0;
+}
